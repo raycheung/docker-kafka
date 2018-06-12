6d2729eb05bb418f58beb3217a85d2b275e80fbc
Ray Cheung <ray.cheung@segantii.com>
2018-06-12 16:45:42 +0800

    Move server.properties template to a separate dir
    This helps syntax highlighting during editing.

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ IP=$(grep "\s${HOSTNAME}$" /etc/hosts | head -n 1 | awk '{print $1}')
 # string with multiple ZooKeeper hosts
 [ -z "$ZOOKEEPER_CONNECTION_STRING" ] && ZOOKEEPER_CONNECTION_STRING="${ZOOKEEPER_IP}:${ZOOKEEPER_PORT:-2181}"
 
-cat /kafka/config/server.properties.template | sed \
+cat /kafka/config/templates/server.properties | sed \
   -e "s|{{KAFKA_ADVERTISED_HOST_NAME}}|${KAFKA_ADVERTISED_HOST_NAME:-$IP}|g" \
   -e "s|{{KAFKA_ADVERTISED_PORT}}|${KAFKA_ADVERTISED_PORT:-9092}|g" \
   -e "s|{{KAFKA_AUTO_CREATE_TOPICS_ENABLE}}|${KAFKA_AUTO_CREATE_TOPICS_ENABLE:-true}|g" \
